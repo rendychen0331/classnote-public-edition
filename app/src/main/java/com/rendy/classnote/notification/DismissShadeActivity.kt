588de19feb6781426fd92ed590dd8c607471da70
rendy <rendychen0331@gmail.com>
2026-05-08 15:25:20 +0800

    feat: DismissShadeActivity 加入 SYSTEM_ALERT_WINDOW 權限檢查，未授權時跳設定頁

--- app/src/main/java/com/rendy/classnote/notification/DismissShadeActivity.kt
+++ app/src/main/java/com/rendy/classnote/notification/DismissShadeActivity.kt
@@ -2,14 +2,21 @@ package com.rendy.classnote.notification
 
 import android.app.Activity
 import android.content.Intent
+import android.net.Uri
 import android.os.Bundle
+import android.provider.Settings
 
 class DismissShadeActivity : Activity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        startForegroundService(Intent(this, FloatingQuickAddService::class.java).apply {
-            action = FloatingQuickAddService.ACTION_SHOW
-        })
+        if (Settings.canDrawOverlays(this)) {
+            startForegroundService(Intent(this, FloatingQuickAddService::class.java).apply {
+                action = FloatingQuickAddService.ACTION_SHOW
+            })
+        } else {
+            startActivity(Intent(Settings.ACTION_MANAGE_OVERLAY_PERMISSION,
+                Uri.parse("package:$packageName")))
+        }
         finish()
     }
 }
